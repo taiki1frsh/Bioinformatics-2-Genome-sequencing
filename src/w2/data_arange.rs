38e5992021bfedb4c4a0cd4debdc2b6c8c52bd9e
taiki-furu <taiki.furusyo@gmail.com>
2021-11-26 20:41:01 +0900

    fix: return number of elements additionaly

--- src/w2/data_arange.rs
+++ src/w2/data_arange.rs
@@ -1,35 +1,36 @@
 use std::env;
 
-pub fn arange_for_eulerian_cycle() -> (Vec<i32>, Vec<Vec<i32>>) {
+// require the command line argument in this fn
+pub fn arange_for_eulerian_cycle() -> (Vec<i32>, Vec<Vec<i32>>, i32) {
     let args: Vec<String> = env::args().collect();
     let path = &args[1];
     let f = std::fs::File::open(path).unwrap();
     let reader = std::io::BufReader::new(f);
     let mut lines = std::io::BufRead::lines(reader).into_iter();
 
-    //let pattern = lines.next().unwrap().unwrap();
-    let mut sub_set_of_kmers: Vec<String> = lines
+    let mut node_elements: Vec<String> = lines
         .map(|x| x.unwrap())
         .collect();
 
-    println!("{:?}", sub_set_of_kmers);
-    let mut vec1: Vec<i32> = Vec::new();
-    let mut vec2 = Vec::new();
+    let mut in_vec: Vec<i32> = Vec::new();
+    let mut out_vec = Vec::new();
     let mut count1 = 0;
     let mut count2 = 0;
-    for i in 0..sub_set_of_kmers.len() {
+    let mut num_element = 0;
+    for i in 0..node_elements.len() {
         if i % 2 == 0 {
-            let num: i32 = sub_set_of_kmers[i + 1].parse().unwrap_or(0);
-            vec1.insert(count1, num);
+            let num: i32 = node_elements[i + 1].parse().unwrap_or(0);
+            in_vec.insert(count1, num);
             count1 += 1;
         } else {
-            let vec_str: Vec<i32> = sub_set_of_kmers[i]
+            let vec_str: Vec<i32> = node_elements[i]
                 .split(' ')
                 .map(|x| x.parse().unwrap_or(0) as i32)
                 .collect();           
-            vec2.insert(count2, vec_str);
-        count2 += 1;
+            out_vec.insert(count2, vec_str);
+            count2 += 1;
+            num_element += vec_str.len();
         }
     }
-    (vec1, vec2)
+    (in_vec, out_vec, num_element as i32)
 }
