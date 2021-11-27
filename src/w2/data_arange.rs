2124fb8a88cfcbd5112355432f146799788ef2a2
taiki-furu <taiki.furusyo@gmail.com>
2021-11-27 15:24:55 +0900

    fix: added a type of returning hashmap type nodes information

--- src/w2/data_arange.rs
+++ src/w2/data_arange.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{collections::{BTreeMap, HashMap}, env};
 
 // require the command line argument in this fn
 pub fn arange_for_eulerian_cycle() -> (Vec<i32>, Vec<Vec<i32>>, i32) {
@@ -23,15 +23,44 @@ pub fn arange_for_eulerian_cycle() -> (Vec<i32>, Vec<Vec<i32>>, i32) {
             in_vec.insert(count1, num);
             count1 += 1;
         } else {
-            let vec_str: Vec<i32> = node_elements[i]
+            let vec: Vec<i32> = node_elements[i]
                 .split(' ')
                 .map(|x| x.parse().unwrap_or(0) as i32)
                 .collect();           
-            num_element += vec_str.len();
-            out_vec.insert(count2, vec_str);
+            num_element += vec.len();
+            out_vec.insert(count2, vec);
             count2 += 1;
             
         }
     }
     (in_vec, out_vec, num_element as i32)
+}
+
+pub fn arange_for_eulerian_cycle_hashmap() -> (BTreeMap<i32, Vec<i32>>, usize) {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    let f = std::fs::File::open(path).unwrap();
+    let reader = std::io::BufReader::new(f);
+    let mut lines = std::io::BufRead::lines(reader).into_iter();
+
+    let mut node_elements: Vec<String> = lines
+        .map(|x| x.unwrap())
+        .collect();
+
+    let mut map= BTreeMap::new();
+    let mut num_element = 0;
+    for i in 0..node_elements.len() {
+        if i % 2 == 0 {
+            let num: i32 = node_elements[i].parse().unwrap_or(0);
+            let vec: Vec<i32> = node_elements[i + 1]
+                .split(' ')
+                .map(|x| x.parse().unwrap_or(0) as i32)
+                .collect();           
+            num_element += vec.len();
+            map.insert(num, vec);
+        } else {
+            continue;
+        }
+    }
+    (map, num_element)
 }
